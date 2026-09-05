fd7b4bedd1baf191d897db8df9fbdefc3ef3b55f
Win Win Aung sein <winwinaungsein@gmail.com>
2026-09-05 20:47:27 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+   root to: 'prototypes#index'
+   
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update] do 
     resources :comments, only: :create
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  root to: 'prototypes#index'
-end
+  # root "posts#index
  resources :users, only: :show
 end
